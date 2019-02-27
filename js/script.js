bbdc15e662af52e081b4e2cba06a58dfb92c4687
Ni-c0de-mus <44680555+Ni-c0de-mus@users.noreply.github.com>
2019-02-27 14:40:05 +0000

    2nd commit

---
+++ js/script.js
@@ -0,0 +1,49 @@
+console.log('test');
+
+var firstName = 'Nic';
+console.log(firstName);
+
+var person = 'instructor';
+console.log(person);
+
+var person2 = 'teacher';
+console.log(person2);
+
+/* Primitives */
+
+var string = 'Nic is a "teacher"';
+console.log(string);
+
+var string2 = "Nic is an 'instructor'";
+console.log(string2);
+
+var num ='27.23';
+console.log(num);
+
+var num2 = '2';
+console.log(num2);
+
+var num3 = '3';
+console.log(num3);
+
+/* Operators */
+
+var a = 5 + 5;
+console.log(a);
+
+var b = 14 - 3;
+console.log(b);
+
+var c = 143 / 7;
+console.log(c);
+
+var d = 33 * 7;
+console.log(d);
+
+/* Operator Precedence
+*
+
+/* Comparison Operators */
+
+var e = 7;
+var f = 12;
